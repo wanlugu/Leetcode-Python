e75d37cd75228c414203d4d5a89d005394991202
Wanlu Gu <42701876+wanlugu@users.noreply.github.com>
2018-09-27 16:59:01 -0700

    knowledge about tree and define class

---
+++ tree.py
@@ -0,0 +1,41 @@
+# Object-oriented programming:  Java, C++, C#, Python,
+#PHP, JavaScript, Ruby, Perl, Object Pascal, Objective-C,
+#Dart, Swift, Scala, Common Lisp, and Smalltalk.
+# f(),x: f() is method, x is object (property) -- encapsulation
+# -- abstraction (hide detail of complexity)
+# -- inheritance
+# -- polymorphism (many forms)
+
+# create a bianary tree
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+ 
+root = Tree()
+root.data = "root"
+root.left = Tree()
+root.left.data = "left"
+root.right = Tree()
+root.right.data = "right"
+ 
+root.left.left = Tree()
+root.left.left.data = "left 2"
+root.left.right = Tree()
+root.left.right.data = "left-right"
+
+# create root
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def PrintTree(self):
+        print(self.data)
+
+root = Node(10)
+root.PrintTree()
+
+# compare the new value with the parent node
