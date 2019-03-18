96dcc86d5d42c20aa2404407a0b253a697286424
Wanlu Gu <42701876+wanlugu@users.noreply.github.com>
2019-03-18 12:02:12 -0700

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,33 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> class Polygon:
+    def __init__(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = [0 for i in range(no_of_sides)]
+
+    def inputSides(self):
+        self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
+
+    def dispSides(self):
+        for i in range(self.n):
+            print("Side",i+1,"is",self.sides[i])
+
+            
+>>> class Triangle(Polygon):
+    def __init__(self):
+        Polygon.__init__(self,3)
+
+    def findArea(self):
+        a, b, c = self.sides
+        # calculate the semi-perimeter
+        s = (a + b + c) / 2
+        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        print('The area of the triangle is %0.2f' %area)
+
+        
+>>> t = Triangle()
+>>> t.inputSides()
+Enter side 1 : 3
+Enter side 2 : 5
+Enter side 3 : 4
+>>> 
